b96415e1148d92a25cd6880395eaeee7262c6406
wmdanor <wmdanor@gmail.com>
2023-03-05 16:36:19 +0000

    New mock init data

--- src/pages/api/reinit.ts
+++ src/pages/api/reinit.ts
@@ -10,16 +10,40 @@ const handler: NextApiHandler = async (_req, res) => {
   
   const paintings: Omit<Painting, 'id'>[] = [
     {
-      name: 'Bulop',
+      name: 'Trees in the forest',
       price: 50,
       isSold: false,
       createdDate: Date.UTC(2022, 12, 3),
+      author: 'James D.K.',
+      contentDescription: 'This painting represents tress in the forest',
+      materialsDescription: 'The painting was created using oil paint',
     },
     {
-      name: 'Bruh',
+      name: 'Illusion',
       price: 100,
       isSold: false,
       createdDate: Date.UTC(2019, 5, 17),
+      author: 'Hyper',
+      contentDescription: 'Painting in abstract style, people who see it says that it has some elements of optical illusion',
+      materialsDescription: 'Created using watercolor painting',
+    },
+    {
+      name: 'Temptation',
+      price: 129,
+      isSold: true,
+      createdDate: Date.UTC(2023, 3, 2),
+      author: 'Hyper',
+      contentDescription: 'Another one abstract work from Hyper, people are often feeling some kind of strange temptation when they see this work',
+      materialsDescription: 'Created using watercolor painting',
+    },
+    {
+      name: 'Sunrise',
+      price: 75,
+      isSold: false,
+      createdDate: Date.UTC(2020, 6, 29),
+      author: 'James Kor',
+      contentDescription: 'Very unique painting showcasing the sunrise, author created it being inspired by the sunrise he sees every day from his window at home',
+      materialsDescription: 'The only tool used for drawing is pencil',
     }
   ];
 
